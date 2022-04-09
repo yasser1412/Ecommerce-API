c2dafd7327636c1edc51bc501de7e24c408230ac
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2022-04-09 15:11:37 +0200

    Update package.json

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const address = "0.0.0.0:3000"
 
 app.use(bodyParser.json())
 
-app.get('/', function (req: Request, res: Response) {
+app.get('/', function (_req: Request, res: Response) {
     res.send('Hello World!')
 })
 
