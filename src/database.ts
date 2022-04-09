ef9ca0d0cba745e344adb40538aabc84bc4de67e
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2022-04-09 15:32:06 +0200

    database.js

---
+++ src/database.ts
@@ -0,0 +1,28 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DB_HOST, DB_NAME, TEST_DB_NAME, DB_USER, DB_PASSWORD, ENV } = process.env;
+
+let client: Pool | undefined = undefined;
+
+if (ENV == 'dev') {
+  client = new Pool({
+    host: DB_HOST,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+  });
+}
+
+if (ENV === 'test') {
+  client = new Pool({
+    host: DB_HOST,
+    database: TEST_DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+  });
+}
+
+export default client;
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 
 const app: express.Application = express()
-const address = "0.0.0.0:3000"
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 
@@ -11,5 +11,5 @@ app.get('/', function (_req: Request, res: Response) {
 })
 
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
+    console.log(`starting app on: ${PORT}`)
 })
