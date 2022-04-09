91ea2e05a8b660a0762294a28306e7d9a27456a3
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2022-04-09 16:09:34 +0200

    bcrypt util

---
+++ src/utils/bcrypt.ts
@@ -0,0 +1,16 @@
+import {hashSync, compareSync} from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
+
+function hash(password: string): string {
+  const pepper = process.env.BCRYPT_PASSWORD;
+  const rounds = process.env.SALT_ROUNDS;
+  const hash = hashSync(password + pepper, parseInt(rounds as string));
+  return hash;
+}
+function compareHash(password: string, hash_string: string): boolean {
+  const pepper = process.env.BCRYPT_PASSWORD;
+  const hash = compareSync(password + pepper, hash_string);
+  return hash;
+}
+export { hash, compareHash };
