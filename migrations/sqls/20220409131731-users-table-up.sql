5d198cc06504f934bdc119ef00d8b71306f5cc09
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2022-04-11 14:59:32 +0200

    Update 20220409131731-users-table-up.sql

--- migrations/sqls/20220409131731-users-table-up.sql
+++ migrations/sqls/20220409131731-users-table-up.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY, 
     firstname VARCHAR(50), 
     lastname VARCHAR(50), 
-    email VARCHAR(255), 
+    email VARCHAR(255) UNIQUE, 
     password VARCHAR, 
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
